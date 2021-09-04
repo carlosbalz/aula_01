be44f342a4d3fe58fa5c7e61306b78a1bcb9630c
aula Usuario de aula <aula@setrem.com.br>
2021-09-04 09:20:39 -0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========");
+            Console.WriteLine("Calcular Área do Terreno");
+            Console.WriteLine("==========");
+            
+            Console.WriteLine("Informe a largura: ");
+            int largura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o comprimento:");
+            int comprimento = Convert.ToInt32(Console.ReadLine());
+
+            CalculaArea(largura, comprimento);
+        }
+
+        static void CalculaArea(int largura, int comprimento)
+        {
+            int area = largura * comprimento;
+            Console.WriteLine($@"Área = {area} m²");
+            //Console.WriteLine("Bom dia :D");
+        }
+    }
+}
